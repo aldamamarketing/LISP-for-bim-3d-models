d6b6df21f215dc5e03d8eba6426793633c382a2b
daniel aldama <aldamadaniel1984@gmail.com>
2026-06-02 13:51:42 -0300

    fix(web): remove vite-plugin-legacy from astro config

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import legacy from '@vitejs/plugin-legacy';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,13 +16,6 @@ export default defineConfig({
     }
   },
   vite: {
-    plugins: [
-      legacy({
-        targets: ['chrome >= 65'],
-        additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        renderLegacyChunks: true
-      })
-    ],
     build: {
       target: 'chrome65'
     },
